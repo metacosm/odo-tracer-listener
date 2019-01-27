036fc3e49ee931f34cfcad3fdf3b3f83af498632
Chris Laprun <metacosm@gmail.com>
2019-01-27 22:29:37 +0100

    Make tracer log in ~/.odo/plugins/tracer.log as visible side-effect.

--- listener.go
+++ listener.go
@@ -1,20 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"github.com/metacosm/odo-event-api/odo/api/events"
 	"github.com/metacosm/odo-event-api/odo/api/events/plugin"
+	"log"
+	"os"
+	"path/filepath"
 )
+
+var logger *log.Logger
 
 type tracer struct{}
 
 func (t tracer) OnEvent(event events.Event) error {
-	fmt.Printf("got event %v\n", event)
+	logger.Printf("got event %v\n", event)
 	return nil
 }
 
 func (t tracer) OnAbort(abortError events.EventCausedAbortError) {
-	fmt.Printf("abort: %v\n", abortError)
+	logger.Printf("abort: %v\n", abortError)
 }
 
 func (t tracer) Name() string {
@@ -28,6 +32,12 @@ func (t tracer) String() string {
 var Listener tracer
 
 func main() {
+	dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+	file := filepath.Join(dir, "tracer.log")
+	logFile, _ := os.OpenFile(file, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	defer logFile.Close()
+	logger = log.New(logFile, "", log.LstdFlags|log.Lshortfile)
+
 	plug := plugin.NewPlugin(Listener)
 	plug.Serve()
 }
